5a6c065a621f271593636fdaeff2e3ed24003692
LIMBO <kaungsettwin999@gmail.com>
2026-03-10 17:25:53 +0630

    Register all marketplace scrapers
    Added a list of all marketplace scrapers to __init__.py.

--- scrapers/__init__.py
+++ scrapers/__init__.py
@@ -1,1 +1,18 @@
+"""
+scrapers/__init__.py
+Registers all marketplace scrapers.
+"""
+from scrapers.gamsgo import GamsGoScraper
+from scrapers.ggsel import GGselScraper
+from scrapers.plati import PlatiScraper
+from scrapers.peakerr import PeakerrScraper
+from scrapers.cdkeys import CDkeysScraper
+
+ALL_SCRAPERS = [
+    GamsGoScraper(),
+    GGselScraper(),
+    PlatiScraper(),
+    PeakerrScraper(),
+    CDkeysScraper(),
+]
 
